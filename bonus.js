8482798194974170e554ef1cb326a6d7bfc6d3af
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2021-03-17 12:22:15 -0500

    Maruthi_Assignment8

--- bonus.js
+++ bonus.js
@@ -1,30 +1,38 @@
+  
 const http = require('http')    //Pull in a useful node package
-                                
-const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
-const port = process.env.port || 3001               //and the port
+//Try http.
+const hostname = '127.0.0.1'    //Local host
+const port = 3001               //Not assigned
+
+const array1 = ['Amazing', 'Endorsment', 'Eligible', 'Beauty', 'Beast', 'Burning'];
+      for (var i = 0; i < array1.length; i++) {
+        console.log(array1[i]);
+      }
 
 const server =
   http.createServer(            //Creates the response loop
-    (req,res)=> {               //Anonymous function to handle the request
+    (req, res) => {               //Anonymous function to handle the request
 
-        var arr=["active","jokes","innocent","naughty","good","smart","innocent"];
-var min=0;
-var max=arr.length;
-var value=Math.floor(Math.random()*(max-min)+min);
+      min = Math.ceil(0);
+      max = Math.floor(5);
+      const rand = Math.floor(Math.random() * (max - min + 1) + min);
 
-//console.log(value)
+      console.log('rand -> '+ rand);
+      console.log('Array value -> '+ array1[rand]);
+
+
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
-      res.write('<html> <head> <title> webpage for bonus  </title> </head>')
+      res.write('<html> <head> <title> BONUS </title> </head>')
       res.write('<body>')
-     
-      res.write('<h1bonus with random number usage</h1>')
-      res.write(`<p>${arr[value]}</p>`)
-      res.end('</body></html>')
-      //Close the response
-    }                           
-)
+      res.write('<h1>')
+      res.write('List of words')
+      res.write('</h1>\n')
+      res.write(`<b>random value from array given elements -> ${array1[rand]}</b>`)
+      res.end('</body></html>')  //Close the response and provide content
+    }                           //No return needed, we modified the res object we got
+  )
 
 server.listen(port, hostname, () => {   //Start the server
   console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
